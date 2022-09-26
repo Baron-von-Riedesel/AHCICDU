4a1b73bb6721186357809fbf2531d88db0942f18
Baron-von-Riedesel <japhethx@gmail.com>
2022-09-26 09:18:39 +0200

    make source masm compatible

--- make.bat
+++ make.bat
@@ -1,3 +1,6 @@
 @echo off
 rem makes ahcicdu.sys
-jwasm -nologo -bin -Sg -Fl -D?EXTMEM=1 -Fo=ahcicdu.sys ahcicdu.asm
+jwasm -nologo -bin -Sg -Fl -Fo=AHCICDU.SYS ahcicdu.asm
+rem alternately using MS tools
+rem ml -c -Sg -Fl ahcicdu.asm
+rem \msvc\bin\link /tiny ahcicdu.obj, AHCICDU.SYS, ahcicdu.map;
